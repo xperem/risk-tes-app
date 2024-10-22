ae8b40e3b3688c05289011c98735dba56b8e7d1d
escanuit <maxence.pereira.pro@gmail.com>
2024-10-22 23:13:33 +0200

    .env pour enlever en dur l'apelle supabase

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ryndjrsdihpuqsomqztd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5bmRqcnNkaWhwdXFzb21xenRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxMTEyNDcsImV4cCI6MjA0NDY4NzI0N30.ASdRHKbxclnx-fmd1zRiOAxDgOAd7xZrAlU_iHi4cLs';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
